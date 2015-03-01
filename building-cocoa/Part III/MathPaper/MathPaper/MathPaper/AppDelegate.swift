e72f082efeec12e123024a515ecbb3690311b90d
Programus <programus@gmail.com>
2015-03-01 10:50:56 +0800

    added MathPaper project and finished basic settings.

---
+++ building-cocoa/Part III/MathPaper/MathPaper/MathPaper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MathPaper
+//
+//  Created by 王 元 on 15-02-28.
+//  Copyright (c) 2015年 programus. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
