e926ad5426c7df1e301d500423b354407f1927f4
programus <programus@gmail.com>
2015-04-14 22:42:28 +0900

    Finished Lecture 2. Fixed problem caused by Xcode and swift upgrade.

--- stanford-course-itunes/project1-calculator/Calculator/Calculator/ViewController.swift
+++ stanford-course-itunes/project1-calculator/Calculator/Calculator/ViewController.swift
@@ -13,7 +13,54 @@ class ViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
     
     var userIsTypingDigit = false
+    
+    var operandStack = Array<Double>()
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsTypingDigit = false
+        }
+    }
 
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsTypingDigit {
+            enter()
+        }
+        switch operation {
+        case "×":
+            performOperation {$0 * $1}
+        case "÷":
+            performOperation {$1 / $0}
+        case "+":
+            performOperation {$0 + $1}
+        case "−":
+            performOperation {$1 - $0}
+        case "√":
+            performUnaryOperation {sqrt($0)}
+        default:
+            break
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performUnaryOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
         if self.userIsTypingDigit {
@@ -22,6 +69,12 @@ class ViewController: UIViewController {
             display.text = digit
             userIsTypingDigit = true
         }
+    }
+    
+    @IBAction func enter() {
+        userIsTypingDigit = false
+        operandStack.append(displayValue)
+        println("operandStack = \(operandStack)")
     }
 }
 
