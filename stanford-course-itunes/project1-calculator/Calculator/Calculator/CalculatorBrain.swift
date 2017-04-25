5d2bcb5b0b8113a23bc5ec2e9667c3710dc5b5b4
programus <programus@gmail.com>
2017-04-26 00:13:03 +0900

    re-study for iOS 10

--- stanford-course-itunes/project1-calculator/Calculator/Calculator/CalculatorBrain.swift
+++ stanford-course-itunes/project1-calculator/Calculator/Calculator/CalculatorBrain.swift
@@ -7,3 +7,72 @@
 //
 
 import Foundation
+
+struct CalculatorBrain {
+    enum Operation {
+        case constant(Double)
+        case unaryOperation((Double) -> Double)
+        case binaryOperation((Double, Double) -> Double)
+        case equal
+    }
+    
+    struct PendingBinaryOperation {
+        let function: (Double, Double) -> Double
+        let firstOperand: Double
+        func perform(with secondOperand: Double) -> Double {
+            return function(firstOperand, secondOperand)
+        }
+    }
+    
+    var operators = [
+        "π": Operation.constant(Double.pi),
+        "e": Operation.constant(M_E),
+        "+": Operation.binaryOperation(+),
+        "−": Operation.binaryOperation(-),
+        "×": Operation.binaryOperation(*),
+        "÷": Operation.binaryOperation(/),
+        "√": Operation.unaryOperation(sqrt),
+        "cos": Operation.unaryOperation(cos),
+        "+/-": Operation.unaryOperation(-),
+        "=": Operation.equal
+    ]
+    
+    private var accumulator: Double?
+    private var pendingBinaryOperation: PendingBinaryOperation?
+    
+    var result: Double? {
+        get {
+            return self.accumulator
+        }
+    }
+    
+    mutating func setOperand(_ operand: Double) {
+        self.accumulator = operand
+    }
+    
+    mutating func performOperation(_ symbol: String) {
+        guard let operation = self.operators[symbol] else {
+            return
+        }
+        
+        switch operation {
+        case .constant(let value):
+            accumulator = value
+        case .unaryOperation(let function):
+            if accumulator != nil {
+                accumulator = function(accumulator!)
+            }
+        case .binaryOperation(let function):
+            if let operand = accumulator {
+                self.pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: operand)
+                self.accumulator = nil
+            }
+        case .equal:
+            if self.pendingBinaryOperation != nil && self.accumulator != nil {
+                self.accumulator = self.pendingBinaryOperation?.perform(with: self.accumulator!)
+                self.pendingBinaryOperation = nil
+            }
+        }
+    }
+    
+}
--- stanford-course-itunes/project1-calculator/Calculator/Calculator/ViewController.swift
+++ stanford-course-itunes/project1-calculator/Calculator/Calculator/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var result: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +20,48 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    var brain = CalculatorBrain()
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    var resultValue: Double {
+        get {
+            if let text = self.result.text, let res = Double(text) {
+                return res
+            } else {
+                return 0
+            }
+        }
+        set {
+            self.result.text = String(newValue)
+        }
+    }
 
-
+    @IBAction func touchDigit(_ sender: UIButton) {
+        guard let digit = sender.currentTitle else {
+            return
+        }
+        
+        if self.userIsInTheMiddleOfTyping {
+            self.result.text?.append(digit)
+        } else {
+            self.result.text = digit
+            self.userIsInTheMiddleOfTyping = true
+        }
+    }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        if self.userIsInTheMiddleOfTyping {
+            self.brain.setOperand(self.resultValue)
+            self.userIsInTheMiddleOfTyping = false
+        }
+        if let operation = sender.currentTitle {
+            self.brain.performOperation(operation)
+        }
+        if let result = self.brain.result {
+            self.resultValue = result
+        }
+    }
 }
 
